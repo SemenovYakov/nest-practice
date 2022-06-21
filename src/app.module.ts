ef748c79613f575020a0a6d9a2ffd4e524227f03
SemenovYakov <d.simson241100@yandex.ru>
2022-06-21 19:09:59 +0400

    start-project

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test_project',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
